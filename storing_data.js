45f021f483dead5d90d39bb9e9f17beca402587b
TylerJac <sans.gaming235@gmail.com>
2024-06-11 12:59:27 -0400

    Create storing_data.js

---
+++ storing_data.js
@@ -0,0 +1,19 @@
+let groupNames = ['Ryan', ' Easten', ' Joann', ' Ben', 'Tyler']
+
+let food = ['Pizza', ' Ice cream', ' Burger', ' Sushi', ' Ramen', ' Steak', ' Cake', ' Toast', ' Bread', ' Rice']
+
+let statesInfo = [
+    ["California", " Sacramento", " California Quail"],
+    ["Texas", " Austin", " Northern Mockingbird"],
+    ["Florida", " Tallahassee", " Northern Mockingbird"],
+    ["New York", " Albany", " Eastern Bluebird"],
+    ["Illinois", " Springfield", " Northern Cardinal"],
+    ["Pennsylvania", " Harrisburg", " Ruffed Grouse"],
+    ["Ohio", " Columbus", " Northern Cardinal"]
+];
+
+console.log('Group Names: ' + groupNames)
+
+console.log('food: ' + food)
+
+console.log('State Info: ' + statesInfo)
